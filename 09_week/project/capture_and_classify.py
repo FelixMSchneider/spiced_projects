2f922be58490815bff4b463bf40ecfa8554fb953
Felix <felix.m.schneider@gmx.de>
2022-10-29 03:01:59 +0200

    added classification text on image

--- 09_week/project/capture_and_classify.py
+++ 09_week/project/capture_and_classify.py
@@ -1,3 +1,4 @@
+import time
 import sys
 import logging
 import os
@@ -9,11 +10,13 @@ import PIL.Image
 import tensorflow as tf
 # clean classify_folder
 
+classified_folder="./classified"
 classify_folder="./classify"
 os.system("mkdir -p " + classify_folder)
+os.system("mkdir -p " + classified_folder)
 
 def clean_classify_folder():
-    os.system("rm -f classify/*.png") 
+    os.system("rm -f "+classify_folder+"/*.png") 
 
 
 def get_and_preprocess_image(imagefile):
@@ -24,7 +27,7 @@ def get_and_preprocess_image(imagefile):
     xtest=np.array([np.array(pp_im),])
     return xtest
 
-CLASSES = ['shoe', 'spoon', 'book', 'fork'] #os.listdir('./data/Trainimages')
+CLASSES = ['shoe', 'spoon', 'book', 'fork', ] #os.listdir('./data/Trainimages')
 
 model = tf.keras.models.load_model('my_model.h5')
 
@@ -32,8 +35,8 @@ model = tf.keras.models.load_model('my_model.h5')
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 data_gen = tf.keras.preprocessing.image.ImageDataGenerator(preprocessing_function=preprocess_input)
 
-
-
+ind=-1
+loopcnt=0
 
 if __name__ == "__main__":
 
@@ -51,12 +54,12 @@ if __name__ == "__main__":
 
     try:
         # q key not pressed 
-        while key != 'q':
+        while True:
             # Capture frame-by-frame
             ret, frame = webcam.read()
             # fliping the image 
             frame = cv2.flip(frame, 1)
-   
+    
             # draw a [224x224] rectangle into the frame, leave some space for the black border 
             offset = 2
             width = 224
@@ -67,42 +70,66 @@ if __name__ == "__main__":
                           pt2=(x+width+offset, y+width+offset), 
                           color=(0, 0, 0), 
                           thickness=2
-            )     
-            
+            )
+            if ind>=0: 
+                # variables defined below! (previous loop after prediction)    
+                frame=cv2.putText(frame, text, org, font, fontScale, color, thickness) 
+                loopcnt+=1
+                if loopcnt>100: # let text be displayed for 100 loops (may be adjusted)
+                    loopcnt=0
+                    ind=-1
+
+   
+            # disable ugly toolbar
+            cv2.namedWindow('frame', flags=cv2.WINDOW_GUI_NORMAL)              
+    
             # get key event
             key = key_action()
             
-            if key == 'space':
-                # write the image without overlay
-                # extract the [224x224] rectangle out of it
-                image = frame[y:y+width, x:x+width, :]
-                write_image(classify_folder, image) 
-
             if key == 'p':
-
+                
                 clean_classify_folder()
                 image = frame[y:y+width, x:x+width, :]
                 write_image(classify_folder, image)
-
+    
                 import glob
                 imlist=glob.glob(classify_folder+"/*.png")
-
+    
                 xtest=get_and_preprocess_image(imlist[0])
-
+    
                 pred=model.predict(xtest, verbose = 0)
                 ind=np.argmax(pred[0])
-                print(" what you are showing is most probably a "+ CLASSES[ind])
+    
+                print(" what you are showing is most probably a "+ CLASSES[ind].upper())
+    
+                # setup text
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                text = CLASSES[ind].upper()
+                fontScale = 3
+                thickness = 2
+                color = (255, 0, 0)
+    
+                # get boundary of this text
+                textsize = cv2.getTextSize(text, font, fontScale, thickness)[0]
+    
+                # get coords based on boundary
+                textX = int(160+122 - textsize[0]/2)
+                textY = int(120+112 + textsize[1]/2)
+    
+                org = (textX, textY)
+                newframe=cv2.putText(frame, text, org, font, fontScale, color, thickness) 
+                write_image(classified_folder, newframe)
+                
+            elif key == 'q':
+                break
 
-
-
-            # disable ugly toolbar
-            cv2.namedWindow('frame', flags=cv2.WINDOW_GUI_NORMAL)              
-            
-            # display the resulting frame
-            cv2.imshow('frame', frame)            
-            
+            else:    
+                cv2.imshow('frame', frame)            
+    
     finally:
         # when everything done, release the capture
+
         logging.info('quit webcam')
         webcam.release()
         cv2.destroyAllWindows()
+
